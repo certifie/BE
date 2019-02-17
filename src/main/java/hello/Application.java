9f67cfcc8baf1cc3507d64091d6c6d2ba624681d
AndreyBelonog <belonogandrey@gmail.com>
2019-02-17 23:19:31 +0200

    Initial commit: created simple RESTfull application with minor logic adding

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,30 @@
+/*
+ * ====================================================================
+ *
+ * Follett Software Company
+ *
+ * Copyright (c) 2019 Follett Software Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is not permitted without a written agreement
+ * from Follett Software Company.
+ *
+ * ====================================================================
+ */
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Follett Software Company
+ * @copyright 2017
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,36 @@
+/*
+ * ====================================================================
+ *
+ * Follett Software Company
+ *
+ * Copyright (c) 2019 Follett Software Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is not permitted without a written agreement
+ * from Follett Software Company.
+ *
+ * ====================================================================
+ */
+package hello;
+
+/**
+ * @author Follett Software Company
+ * @copyright 2017
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public String getContent() {
+        return content;
+    }
+}
